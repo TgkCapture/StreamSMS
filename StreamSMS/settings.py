6be65ce62b1aa7f78ff25e68704708cc88e1fced
TgkCapture <tawongakanyenda5@gmail.com>
2024-06-06 22:57:09 +0200

    feat: setting up homepage

--- StreamSMS/settings.py
+++ StreamSMS/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'StreamSMS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'StreamSMS' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- StreamSMS/urls.py
+++ StreamSMS/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import homepage
 
 urlpatterns = [
+    path('', homepage, name='homepage'),
     path('admin/', admin.site.urls),
     path('messages/', include('messages_app.urls')),
 ]
