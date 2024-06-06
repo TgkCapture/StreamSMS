a39b64e886abccbdfabbd99188a488251ceb6c94
TgkCapture <tawongakanyenda5@gmail.com>
2024-06-06 08:32:24 +0200

    feat:  added table for messages

--- messages/models.py
+++ messages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    from_number = models.CharField(max_length=15)
+    message_body = models.TextField()
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.from_number}: {self.message_body}"
