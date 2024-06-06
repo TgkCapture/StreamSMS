3d699d11d73dce77c116dbefae95b52673130cbb
TgkCapture <tawongakanyenda5@gmail.com>
2024-06-06 08:36:05 +0200

    feat: sms webhook

--- messages/views.py
+++ messages/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Message
+import datetime
 
-# Create your views here.
+@csrf_exempt
+def sms_webhook(request):
+    if request.method == 'POST':
+        from_number = request.POST.get('From')
+        message_body = request.POST.get('Body')
+        Message.objects.create(from_number=from_number, message_body=message_body, approved=False)
+        return HttpResponse("Message received", status=200)
+    return HttpResponse(status=405)
