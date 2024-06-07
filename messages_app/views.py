5f4e6efb72ec6795ad15d00d20c8174c7fdc135f
TgkCapture <tawongakanyenda5@gmail.com>
2024-06-07 16:23:01 +0200

    feat : changed rss feed format from xml to json

--- messages_app/views.py
+++ messages_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Message
 import datetime
@@ -30,24 +30,27 @@ def decline_message(request, id):
 
 def generate_rss(request):
     approved_messages = Message.objects.filter(approved=True)
-    rss_feed = '<?xml version="1.0" encoding="UTF-8" ?>'
-    rss_feed += '<rss version="2.0">'
-    rss_feed += '<channel>'
-    rss_feed += '<title>Approved Messages</title>'
-    rss_feed += '<link>http://127.0.0.1:8000/messages/rss</link>'
-    rss_feed += '<description>Approved SMS messages</description>'
-    
+    items = []
+
     for message in approved_messages:
-        rss_feed += '<item>'
-        rss_feed += f'<title>{message.from_number}</title>'
-        rss_feed += f'<description>{message.message_body}</description>'
-        rss_feed += f'<pubDate>{datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S +0000")}</pubDate>'
-        rss_feed += '</item>'
+        items.append({
+            'sender': message.from_number,
+            'message': message.message_body,
+            'link': f"http://127.0.0.1:8000/messages/{message.id}",
+            # 'pubDate': datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S +0000")
+        })
     
-    rss_feed += '</channel>'
-    rss_feed += '</rss>'
+    rss_feed_json = {
+        'version': '2.0',
+        'channel': {
+            'title': 'Approved Messages',
+            'link': 'http://127.0.0.1:8000/messages/rss',
+            'description': 'Approved SMS messages',
+            'items': items
+        }
+    }
     
-    return HttpResponse(rss_feed, content_type='application/rss+xml')
+    return JsonResponse(rss_feed_json)
 
 def messages_list(request):
     messages = Message.objects.all()
