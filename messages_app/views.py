8e9b2e2ce1e4fd9cbdd51236887bb2f42c66dc3c
TgkCapture <tawongakanyenda5@gmail.com>
2024-06-09 22:03:28 +0200

    fix: UnorderedObjectListWarning on message listing

--- messages_app/views.py
+++ messages_app/views.py
@@ -20,8 +20,8 @@ def africastalking_webhook(request):
 
 @login_required
 def moderation_interface(request):
-    messages_list = Message.objects.filter(approved=False, declined=False)
-    paginator = Paginator(messages_list, 15)
+    messages_list = Message.objects.filter(approved=False, declined=False).order_by('-created_at')
+    paginator = Paginator(messages_list, 13)
 
     page_number = request.GET.get('page')
     messages = paginator.get_page(page_number)
@@ -70,7 +70,7 @@ def generate_rss(request):
 
 @login_required
 def messages_list(request):
-    messages_list = Message.objects.all()
+    messages_list = Message.objects.all().order_by('-created_at')
     paginator = Paginator(messages_list, 13)
 
     page_number = request.GET.get('page')
