a05816b0cbe7b0b311fb327c7053f9915971f1c2
TgkCapture <tawongakanyenda5@gmail.com>
2024-06-09 20:36:18 +0200

    feat: introduced soft delete

--- messages_app/models.py
+++ messages_app/models.py
@@ -5,6 +5,7 @@ class Message(models.Model):
     from_number = models.CharField(max_length=15)
     message_body = models.TextField()
     approved = models.BooleanField(default=False)
+    declined = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     history = HistoricalRecords()
 
