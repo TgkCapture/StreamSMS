3cfe81ed53e79f72942e3a9c9bec384939acbc7e
TgkCapture <tawongakanyenda5@gmail.com>
2024-06-09 19:49:10 +0200

    feat: audting user actions

--- messages_app/models.py
+++ messages_app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from simple_history.models import HistoricalRecords
 
 class Message(models.Model):
     from_number = models.CharField(max_length=15)
     message_body = models.TextField()
     approved = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    history = HistoricalRecords()
 
     def __str__(self):
         return f"{self.from_number}: {self.message_body}"
