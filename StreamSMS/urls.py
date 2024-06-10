33fe414fd188fd31810517ad2518abd66f9b3396
TgkCapture <tawongakanyenda5@gmail.com>
2024-06-10 10:32:13 +0200

    feat: separated rss formats into two

--- StreamSMS/urls.py
+++ StreamSMS/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import homepage, profile
-from messages_app.views import generate_rss
+from messages_app.views import generate_rss_json, generate_rss
 from django.contrib.auth import views as auth_views
 from messages_app.views import africastalking_webhook
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', homepage, name='homepage'),
     path('admin/', admin.site.urls),
     path('rss/', generate_rss, name='generate_rss'),
+    path('rss-json/', generate_rss_json, name='generate_rss_json'),
     path('messages/', include('messages_app.urls')),
     path('profile/', profile, name='profile'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
