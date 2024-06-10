3b6c052e6fd7d48060d02a159f0ccf4ada0b5b5d
TgkCapture <tawongakanyenda5@gmail.com>
2024-06-10 10:22:20 +0200

    feat: Added profile page where users can view their profile, settings, groups, and permissions

--- StreamSMS/urls.py
+++ StreamSMS/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import homepage
+from .views import homepage, profile
 from messages_app.views import generate_rss
 from django.contrib.auth import views as auth_views
 from messages_app.views import africastalking_webhook
@@ -10,6 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('rss/', generate_rss, name='generate_rss'),
     path('messages/', include('messages_app.urls')),
+    path('profile/', profile, name='profile'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('africastalking-webhook/', africastalking_webhook, name='africastalking_webhook'),
