e8a40a0620c9d74f9588b1d0b45dc748fea13d09
TgkCapture <tawongakanyenda5@gmail.com>
2025-06-28 10:59:37 +0200

    feat: Improved authentication backend

--- apps/broadcast_manager/backends.py
+++ apps/broadcast_manager/backends.py
@@ -1,15 +1,26 @@
 # broadcast_manager/backends.py
 from django.contrib.auth.backends import ModelBackend
-from .models import User
+from django.core.exceptions import PermissionDenied
 
 class RoleBasedAuthBackend(ModelBackend):
     def has_perm(self, user_obj, perm, obj=None):
-        if user_obj.is_admin():
+        # Superusers and admins have all permissions
+        if user_obj.is_superuser or user_obj.is_admin():
             return True
-        elif user_obj.is_editor():
-            return True
-        elif user_obj.is_producer():
-            return True
-        elif user_obj.is_fcc():
-            return True
+            
+        # Handle app-specific permissions
+        if perm.startswith('broadcast_manager.'):
+            if perm.endswith('nominate_candidates'):
+                return user_obj.can_nominate
+            elif perm.endswith('vote_in_polls'):
+                return user_obj.can_vote
+            elif perm.endswith('moderate_content'):
+                return user_obj.can_moderate
+                
         return super().has_perm(user_obj, perm, obj)
+    
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        user = super().authenticate(request, username, password, **kwargs)
+        if user and not user.is_active:
+            raise PermissionDenied("Account is disabled")
+        return user
